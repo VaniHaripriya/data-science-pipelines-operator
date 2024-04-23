48345167b5e979a4405ac7a8e9ce8e89cc155c8e
vmudadla <vmudadla@redhat.com>
2024-04-23 13:07:55 -0500

    Update pipeline

--- tests/resources/test-pipeline-with-custom-pip-server.py
+++ tests/resources/test-pipeline-with-custom-pip-server.py
@@ -3,7 +3,7 @@ from kfp import dsl, compiler
 
 @dsl.component(base_image="quay.io/opendatahub/ds-pipelines-ci-executor-image:v1.0",
 packages_to_install=['boto3'],
-pip_index_urls=['http://pypi-server.test-pypiserver.svc.cluster.local/'])
+pip_index_urls=['http://pypi-server.test-pypiserver.svc.cluster.local:8080'])
 def say_hello(name: str) -> str:
     hello_text = f'Hello, {name}!'
     print(hello_text)
