54664f4bcd0b75757d00104d28067ba62017dfb1
vmudadla <vmudadla@redhat.com>
2024-05-29 13:43:02 -0500

    Update test

--- tests/pipeline_runs_test.go
+++ tests/pipeline_runs_test.go
@@ -31,7 +31,6 @@ import (
 )
 
 func (suite *IntegrationTestSuite) TestPipelineSuccessfulRun() {
-	var pipelineName string
 
 	suite.T().Run("Should create a Pipeline Run", func(t *testing.T) {
 		// Retrieve Pipeline ID to create a new run
@@ -55,8 +54,7 @@ func (suite *IntegrationTestSuite) TestPipelineSuccessfulRun() {
 
 	suite.T().Run("Should create a Pipeline Run using custom pip server", func(t *testing.T) {
 		// Retrieve Pipeline ID to create a new run
-		loggr.Info(pipelineName)
-		pipelineDisplayName := pipelineName
+		pipelineDisplayName := "test-pipeline-with-custom-pip-server-run.yaml"
 		loggr.Info(pipelineName)
 		pipelineID := TestUtil.RetrievePipelineId(t, APIServerURL, pipelineDisplayName)
 		postUrl := fmt.Sprintf("%s/apis/v2beta1/runs", APIServerURL)
